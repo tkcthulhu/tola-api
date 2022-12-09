f2b4a492d019b4a43e12aa190e560625c85dbdcb
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-12-09 17:50:05 +0000

    Fix the thing

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from tola import views, urls
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.CustomUserViewSet)
