795fae9109969d47e4bae8c8d78019f3654fba0e
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-12-12 16:13:50 +0000

    routes secured

--- api/settings.py
+++ api/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
